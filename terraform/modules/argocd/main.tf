6d2f314938d89f584309b791b1759a0ac058b7f1
miyamo2 <miyamo2@outlook.com>
2025-11-07 03:17:03 +0900

    fix: argocd cm

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -1,7 +1,7 @@
 locals {
-  argocd_cm_patch_data = [
+  argocd_cm_patch_data = toset([
     for v in var.tenants: templatefile("${path.module}/argocd-cm_patch.yaml.tftpl", { tenant = v })
-  ]
+  ])
   argocd_rbac_cm_patch_data = templatefile("${path.module}/argocd-rbac-cm_patch.yaml.tftpl", {
     tenants = var.tenants
   })
@@ -9,7 +9,7 @@ locals {
 
 resource "terraform_data" "argocd_cm" {
   for_each = local.argocd_cm_patch_data
-  triggers_replace = each.value
+  triggers_replace = local.argocd_cm_patch_data
   provisioner "local-exec" {
     command = <<EOF
     export KUBECONFIG=${pathexpand(var.kube_config)}
