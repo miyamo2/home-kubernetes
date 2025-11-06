076a0cf6b4b15e2ec33bcb7c989ff3ca03e88b40
miyamo2 <miyamo2@outlook.com>
2025-11-07 03:15:44 +0900

    fix: argocd cm

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -1,24 +1,29 @@
 locals {
-  argocd_cm_patch_date      = jsonencode(jsondecode(templatefile("${path.module}/argocd-cm_patch.yaml.tftpl", { tenants = var.tenants })))
-  argocd_rbac_cm_patch_date = templatefile("${path.module}/argocd-rbac-cm_patch.yaml.tftpl", { tenants = var.tenants })
+  argocd_cm_patch_data = [
+    for v in var.tenants: templatefile("${path.module}/argocd-cm_patch.yaml.tftpl", { tenant = v })
+  ]
+  argocd_rbac_cm_patch_data = templatefile("${path.module}/argocd-rbac-cm_patch.yaml.tftpl", {
+    tenants = var.tenants
+  })
 }
 
 resource "terraform_data" "argocd_cm" {
-  triggers_replace = local.argocd_cm_patch_date
+  for_each = local.argocd_cm_patch_data
+  triggers_replace = each.value
   provisioner "local-exec" {
     command = <<EOF
     export KUBECONFIG=${pathexpand(var.kube_config)}
-    kubectl patch cm argocd-cm --type merge -p '${local.argocd_cm_patch_date}' --context ${var.kube_context} -n argocd
+    kubectl patch cm argocd-cm --type merge -p '${each.value}' --context ${var.kube_context} -n argocd
     EOF
   }
 }
 
 resource "terraform_data" "argocd_rbac_cm" {
-  triggers_replace = local.argocd_rbac_cm_patch_date
+  triggers_replace = local.argocd_rbac_cm_patch_data
   provisioner "local-exec" {
     command = <<EOF
     export KUBECONFIG=${pathexpand(var.kube_config)}
-    kubectl patch cm argocd-rbac-cm --type merge -p '${local.argocd_rbac_cm_patch_date}' --context ${var.kube_context} -n argocd
+    kubectl patch cm argocd-rbac-cm --type merge -p '${local.argocd_rbac_cm_patch_data}' --context ${var.kube_context} -n argocd
     EOF
   }
 }
