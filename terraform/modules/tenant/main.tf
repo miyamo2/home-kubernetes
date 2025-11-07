2c6c0f2d03e10bbe01ffa549dc78ed9c7bfd601c
miyamo2 <miyamo2@outlook.com>
2025-11-07 15:12:17 +0900

    fix: rbac related keda

--- terraform/modules/tenant/main.tf
+++ terraform/modules/tenant/main.tf
@@ -199,7 +199,7 @@ resource "kubernetes_cluster_role_binding_v1" "keda" {
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
+    kind      = "ClusterRole"
     name      = "keda-clustertriggerauthentications-readonly"
   }
   subject {
