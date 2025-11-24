a9dad7b683f77796c59e16f5934ca4ab8f017ba9
miyamo2 <miyamo2@outlook.com>
2025-11-24 13:18:17 +0900

    fix: keda

--- terraform/keda.tf
+++ terraform/keda.tf
@@ -3,23 +3,12 @@ resource "helm_release" "keda" {
   chart            = "keda"
   namespace        = "keda"
   repository       = "https://kedacore.github.io/charts"
-  version          = "2.18.0"
   create_namespace = true
   timeout          = 500
 
   depends_on = [
     terraform_data.wait_cilium_ready
   ]
-
-  set {
-    name  = "image.keda.tag"
-    value = "2.18.0@sha256:ab9f9ccbf8e6736e19255d8344c8ad57a59d469877c64d157dfb089cbfc2ddef"
-  }
-
-  set {
-    name  = "logging.operator.level"
-    value = "debug"
-  }
 }
 
 resource "kubernetes_cluster_role" "keda_clustertriggerauthentications_readonly" {
